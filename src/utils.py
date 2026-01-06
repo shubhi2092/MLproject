db6d8997325400e1b3f25f36904cd708369b5d26
shubhi2092 <104774342+shubhi2092@users.noreply.github.com>
2026-01-06 23:25:24 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -21,13 +22,20 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
         for i in range (len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
             
+            grid = GridSearchCV(estimator=model,param_grid=para,cv=3)
+            grid.fit(x_train,y_train)
+            
+           
+            model.set_params(**grid.best_params_)
             model.fit(x_train,y_train)
+            
             
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -35,7 +43,7 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
             
-            report[list(models.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = (test_model_score,grid.best_params_)
             
             return report
         
