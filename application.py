bbe520fd8e7b2eebbce5e6bff82b1a7aafe2b636
shubhi2092 <104774342+shubhi2092@users.noreply.github.com>
2026-01-18 21:58:01 +0530

    COMMIT 5

---
+++ application.py
@@ -0,0 +1,42 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.Pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application = Flask(__name__)
+
+app=application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predicteddata',methods=['GET','POST'])
+def predicted_datapoints():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+        
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        
+        predict_pipeline=PredictPipeline()
+        result=predict_pipeline.predict(pred_df)
+        return render_template('home.html',results=result[0])
+    
+    
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)
+    
